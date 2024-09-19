ece5d3805e9f58812c9de0e4abb40e965ed53801
Shubham Somani <shubham.somani@toptal.com>
2024-09-19 19:42:46 +0530

    added video

--- src/pages/parent/step-one-generic.vue
+++ src/pages/parent/step-one-generic.vue
@@ -36,15 +36,22 @@
               must be scheduled through our website.
             </p>
 
+            <q-card-section>
+              <iframe
+                src="https://www.veed.io/embed/57936041-a2de-4dd6-ae2c-80f63b167a16?watermark=1&color=&sharing=1&title=1"
+                width="100%" height="500" frameborder="0" title="Chancy And Bruce " webkitallowfullscreen
+                mozallowfullscreen allowfullscreen></iframe>
+            </q-card-section>
+
             <p>
               For more information, visit <a target="_blank"
                 href="https://www.chancyandbruce-ra.com">chancyandbruce-ra.com</a> or contact your School
               Director/Principal.
               <br />
-              <a target="_blank"
+              <!-- <a target="_blank"
                 href="https://drive.google.com/file/d/1v-OIDy9mnS0W1lPYsTA8c02f8f3DmABR/view?usp=sharing">
                 Read literature regarding Chancy and Bruce.
-              </a>
+              </a> -->
             </p>
 
             <q-banner v-if="isPaymentRequired" class="bg-banner-error">
